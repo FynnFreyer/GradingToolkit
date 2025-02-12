94607fe344bb3a72703aab8aeabd89608fcb0cf9
Fynn Freyer <fynn@fynns.site>
2025-02-12 13:45:22 +0100

    build: add project info

---
+++ src/github_classroom_toolkit/__about__.py
@@ -0,0 +1,12 @@
+__name__ = "github_classroom_toolkit"
+__description__ = "Tools for automating grading of java exercises in GitHub classroom."
+__version__ = "0.0.1dev0"
+
+__authors__ = [
+    {"name": "Fynn Freyer"},
+    {"name": "Delia Morgan"},
+]
+
+__maintainers__ = [
+    {"name": "Fynn Freyer", "email": "fynn.freyer@googlemail.com"},
+]
