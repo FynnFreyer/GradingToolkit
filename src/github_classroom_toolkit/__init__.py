d03f76e813f3cfe951a6311a34400151ccbe2dc9
Fynn Freyer <fynn.freyer@googlemail.com>
2025-02-17 16:50:30 +0100

    refactor: remove autograde module and move docstring to package __init__

---
+++ src/github_classroom_toolkit/__init__.py
@@ -0,0 +1,6 @@
+"""
+A not so quick, but very much dirty auto-grading script for Java projects using Gradle and Junit5.
+
+This can be used to locally test and grade student repositories,
+in case no free minutes are available for running GitHub actions.
+"""
