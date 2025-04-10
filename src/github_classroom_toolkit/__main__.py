f22925c396533ab922440c027829fd947320cdf9
Fynn Freyer <fynn.freyer@googlemail.com>
2025-04-10 13:00:13 +0200

    refactor(__main__): directly use models in argparse types and refactor logic accordingly
    also, adds the logic for grading assignments

--- src/github_classroom_toolkit/__main__.py
+++ src/github_classroom_toolkit/__main__.py
@@ -2,8 +2,8 @@ from argparse import ArgumentParser, Namespace
 from pathlib import Path
 from typing import Sequence
 
-from github_classroom_toolkit.model.course import Course, Grade, Student, Submission
-from github_classroom_toolkit.model.github import Classroom
+from github_classroom_toolkit.model.course import Course, Grade, Student
+from github_classroom_toolkit.model.github import Classroom, Assignment
 
 
 def parse_args(args: Sequence[str] | None = None) -> Namespace:
@@ -11,12 +11,12 @@ def parse_args(args: Sequence[str] | None = None) -> Namespace:
 
     # mutex group classroom/assignment
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument("-c", "--classroom", type=int,
+    group.add_argument("-c", "--classroom", type=Classroom.from_id,
                         help="the ID of the GitHub classroom to grade")
-    group.add_argument("-a", "--assignment", type=int,
+    group.add_argument("-a", "--assignment", type=Assignment.from_id,
                         help="the ID of the GitHub assignment to grade")
 
-    parser.add_argument("-s", "--students", type=Path, required=True,
+    parser.add_argument("-s", "--students", type=Student.from_student_data, required=True,
                         help="path to a CSV with names and GitHub accounts of students")
 
     return parser.parse_args(args)
@@ -26,10 +26,19 @@ def main(args: Namespace | None = None):
     args = args or parse_args()
 
     if args.classroom:
-        course = Course.from_classroom_and_students(args.classroom, args.students)
-        course.grade_course("all_grades.csv")
+        course = Course(args.classroom, args.students)
+        results = Grade.grade_course(course)
+        out = f"grades_classroom_{course.classroom.id}.csv"
     elif args.assignment:
-        raise NotImplementedError("Grading individual assignments is not supported yet")
+        assignment: Assignment = args.assignment
+        out = f"grades_assignment_{assignment.id}.csv"
+        results = Grade.grade_assignment(args.assignment)
+    else:
+        # should never happen bc of mutex group, but keeps the IDE happy
+        raise NotImplementedError("Only grading classrooms and assignments is supported")
+
+    grades = Grade.tabulate_results(results)
+    grades.reset_index().to_csv(out, index=False)
 
 
 if __name__ == "__main__":
